0c36aaf3480dae0499727fd0d02ed97f43935fa2
insiememultiservizi-coop <insiememultiservizi@gmail.com>
2026-04-01 11:10:24 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,31 @@
-const CACHE = 'timbratura-v1';
+const CACHE_NAME = 'timbratura-v1';
+const ASSETS = [
+  '/timbratura-app/index.html',
+  '/timbratura-app/admin.html'
+];
 
 self.addEventListener('install', e => {
-  self.skipWaiting();
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([
-      './index.html',
-      './manifest.json',
-      './icons/icon-192.png',
-      './icons/icon-512.png'
-    ]))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  self.clients.claim();
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-  const url = e.request.url;
-  // Lascia passare Firebase e Google Fonts senza cache
-  if (url.includes('firebase') || url.includes('googleapis') || url.includes('gstatic')) {
+  if (e.request.url.includes('firebase') || e.request.url.includes('googleapis')) {
+    e.respondWith(fetch(e.request));
     return;
   }
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
